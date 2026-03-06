de57826ef9820d2a67e472d72259977958e9cee1
Randy Eckman <emanspeaks@gmail.com>
2026-03-06 14:12:20 -0600

    prompt for root password at beginning of script

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+read -sp "Enter root password: " ROOT_PASSWORD
+echo
+
 # block size is 512 bytes, total blocks is 78140160
 # start block for root partition is block 57408 = 64 + 28*1024*2
 # start block for swap partition is block 71360576 = 57408 + 71303168
@@ -30,7 +33,11 @@ hformat /dev/sda2
 mkfs.ext4 /dev/sda3
 mkswap /dev/sda4
 swapon /dev/sda4
-
+sync
+sync
+sync
+echo
+echo "Disk setup complete. Continuing with installation in 10 seconds..."
 sleep 10  # to ensure i can see any errors in disk setup before proceeding
 
 timedatectl
@@ -83,7 +90,7 @@ echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "orion" > /etc/hostname
-passwd
+echo "$ROOT_PASSWORD" | passwd --stdin root
 ln -s /usr/lib/systemd/network/80-wifi-station.network.example /etc/systemd/network/80-wifi-station.network
 ln -s /usr/lib/systemd/network/89-ethernet.network.example /etc/systemd/network/89-ethernet.network
 systemctl enable sshd
